e3691a0d48262e8d530f95ff570b65c0f8d86c74
Suprabh Jain <official.suprabh@gmail.com>
2024-10-22 11:35:53 +0530

    Updating optimising the images

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { ThemeProvider } from "styled-components";
-import GlobalStyles from "./styles/global";
-import theme from "./styles/theme";
-import { BrowserRouter } from "react-router-dom";
-import Routes from "./routes";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/global';
+import theme from './styles/theme';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/little-lemon">
+    <BrowserRouter basename='/'>
       <ThemeProvider theme={theme}>
         <GlobalStyles />
         <Routes />
       </ThemeProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://suprabh9999.github.io/meta-little-lemon-project',
+  base: 'https://suprabh9999.github.io/',
 });
